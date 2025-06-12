cd026f8268a065d110f3c3ae88f07c1b0261a417
Quentin <2761319+nitneuq6@users.noreply.github.com>
2025-06-12 12:13:21 +0200

    Changed statemachine
    Added transitions, changed cmd positions

--- Bibo-RFID - kopie/Bibo-main/main.c
+++ Bibo-RFID - kopie/Bibo-main/main.c
@@ -7,8 +7,6 @@ Contains state machine for AGV control
 #include <util/delay.h>             // used for RFID scanner
 // -- Custom libraries -- //
 #include "TM1637.h"                 // Display
-#include "HC-SR04.h"                // Ultrasonic sensors
-#include "stepper.h"                // motor control
 #include "config.h"                 // defines with settings
 #include "basic_io.h"               // various in- and outputs
 #include "music.h"                  // Buzzer control
@@ -43,6 +41,7 @@ int main(void)
         //new code
         einde_opdracht,
         s_turn,
+        u_turn,
         backward_driving
     };
 
@@ -54,6 +53,7 @@ int main(void)
 
     /// --- Init --- ///
     // Deze twee moet je in de main (of op de plek waar je de functie roept als dat niet in main is)
+    start_button_init();
     spi_device rfid_left = {&PORTB, PB0, &DDRB};
     spi_device rfid_right = {&PORTL, PL0, &DDRL};
     init();
@@ -67,11 +67,9 @@ int main(void)
         /// --- PACKAGE DETECTED --- ///
 
         case package_detected:
-            // Stop
-            task_manager(stop, standard_speed, standard_acceleration);
+            // AGV should be stopped
             // Reset GP timer
             gp_timer(-1);
-
             if(rfid_tag_detected(rfid_right)){
                 // Add 1 to package count
                 package_metal++;
@@ -85,9 +83,6 @@ int main(void)
                 package_non_metal++;
             }
             }
-
-            // Turn LEDs on
-            led_control(1);
             // Make display extra bright
             display_brightness(MAX_BRIGHTNESS);
             // Play buzzer sound
@@ -101,35 +96,50 @@ int main(void)
             //THIS is for the change_demo_area. at package 5 for non metal
             if (package_non_metal==change_demo_package)
             {
+                // Stop AGV
+                task_manager(stop, standard_speed, standard_acceleration);
+                // Play sound and display end
+                display_end();
+                play_beep();
                 current_state = einde_opdracht;
             }
             else{
+                // Start moving
+                task_manager(forward_fast, standard_speed, standard_acceleration);
+                // Transition
                 current_state = forward;
             }
             break;
         case einde_opdracht: //einde bereikt
-            task_manager(stop, standard_speed, standard_acceleration);
+            // AGV is stopped
             display_end();
-            play_beep();
             break;
         case backward_driving:
             task_manager(backward_fast, standard_speed, standard_acceleration);
             current_state = einde_opdracht;
             break;
         case forward:
-            task_manager(forward_fast, standard_speed, standard_acceleration);//does AGV return an ack here?
+            //task_manager(forward_fast, standard_speed, standard_acceleration);//does AGV return an ack here?
             //while driving look for packages
             if (package_detected_donk){
+                // Stop
+                task_manager(stop, standard_speed, standard_acceleration);
+                // Transition
                 current_state = package_detected;
             }
             //listing for an ACK to know when to make u_turn, this only happens once
             if(USART1_receiveByte()==0x01){
-                display_turn();
-                task_manager(turn_right, standard_speed, standard_acceleration);
+                current_state = u_turn;
             }
-        default:
+        case u_turn:
+            display_turn();
+            task_manager(turn_right, standard_speed, standard_acceleration);
+            break;
+        default: // Should this be starting_state?
             if(starting_button()){
-            task_manager(forward_fast, standard_speed, standard_acceleration);}//does AGV return an ack here?
+            task_manager(forward_fast, standard_speed, standard_acceleration);
+            current_state = forward;
+            }//does AGV return an ack here?
             break;
 
 
