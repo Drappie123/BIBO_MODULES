f3f8c48040ec3b294f5d6c3fef13b7df15608ce4
Quentin <2761319+nitneuq6@users.noreply.github.com>
2025-06-17 17:07:16 +0200

    Finished statemachine for manipulator

--- Bibo-duwmodule - kopie/Bibo-main/main.c
+++ Bibo-duwmodule - kopie/Bibo-main/main.c
@@ -21,8 +21,6 @@ int main(void)
         wait,
         forward,
         weight_detection,
-        light_weight,
-        heavy_weight,
         s_turn,
         reverse,
         end
@@ -35,7 +33,11 @@ int main(void)
 
     enum available_states current_state = wait;
     enum available_sub_states current_sub_state = entry;
-    // First turn 0 is left, 1 is right
+    // Store the first package detection
+    char first_package_detected = 0;
+    // Store number of detected packages
+    int light_packages = 0;
+    int heavy_packages = 0;
 
     /// --- Init --- ///
     init();
@@ -63,17 +65,28 @@ int main(void)
                     gp_timer(-1);
                     break;
                 case running:
+                    // Show package count
+                    display_metal_and_non_metal(heavy_packages, light_packages);
                     // After set time, go slower
                     if(gp_timer(50)){
                         // Slow down
-                        task_manager(forward_fast, 0x0F, standard_acceleration);
+                        task_manager(forward_slow, 0x0F, standard_acceleration);
                         current_sub_state = exit;
+                    }
+                    // Check limit switch in case it comes before the slowdown
+                    if(light_limit_switch_25()){
+                        // Transition
+                        current_state = weight_detection;
+                        current_sub_state = entry;
                     }
                     break;
                 case exit:
+                    // Show package count
+                    display_metal_and_non_metal(heavy_packages, light_packages);
+                    // Check limit switch for package
                     if(light_limit_switch_25()){
                         // Slow down further
-                        task_manager(forward_fast, 0x09, standard_acceleration);
+                        task_manager(forward_slow, 0x09, standard_acceleration);
                         // Transition
                         current_state = weight_detection;
                         current_sub_state = entry;
@@ -86,10 +99,12 @@ int main(void)
                 default:
                 case entry:
                     // Send forward driving command
-                    task_manager(forward_fast, standard_speed, standard_acceleration);
+                    task_manager(backward_fast, standard_speed, standard_acceleration);
                     current_sub_state = running;
                     break;
                 case running:
+                    // Show package count
+                    display_metal_and_non_metal(heavy_packages, light_packages);
                     // Check for ACK at end of path
                     if(USART1_receiveByte()==0x01){
                         // Transition to end
@@ -101,30 +116,63 @@ int main(void)
             break;
         case weight_detection:
             switch(current_sub_state){
-                default:
                 case entry:
+                    // Go extra slow
+                    task_manager(forward_slow, 0x09, standard_acceleration);
+                    current_sub_state = running;
+                    break;
+                case running:
+                    // Show package count
+                    display_metal_and_non_metal(heavy_packages, light_packages);
+                    // Check switches
                     if(heavy_limit_switch_26()){
                         // Stop
                         task_manager(stop, standard_speed, standard_acceleration);
-                        // Add to heavy count
-                        current_sub_state = running;
+                        heavy_packages++;
+                        current_sub_state = exit;
                     }
                     if(gp_timer(5)){
                         // Stop
                         task_manager(stop, standard_speed, standard_acceleration);
-                        // Add to light count
-                        current_sub_state = running;
+                        light_packages++;
+                        current_sub_state = exit;
                     }
                     break;
-                case running:
+                case exit:
+                    // Show package count
+                    display_metal_and_non_metal(heavy_packages, light_packages);
+                    // Transition after set time
                     if(gp_timer(5)){
-                        current_state = s_turn;
-                        current_sub_state = entry;
+                        if(first_package_detected){
+                            current_state = reverse;
+                            current_sub_state = entry;
+                        }
+                        else{
+                            first_package_detected = 1;
+                            current_state = s_turn;
+                            current_sub_state = entry;
+                        }
                     }
                     break;
             }
             break;
         case s_turn:
+            switch(current_sub_state){
+                default:
+                case entry:
+                    task_manager(stop, standard_speed, standard_acceleration);
+                    current_sub_state = running;
+                    break;
+                case running:
+                    display_turn();
+                    // Check for ACK at end s-turn
+                    if(USART1_receiveByte()==0x01){
+                        // Transition to weight detection
+                        current_state = weight_detection;
+                        current_sub_state = entry;
+                    }
+                    break;
+            }
             break;
         case end:
             switch(current_sub_state){
@@ -143,102 +191,8 @@ int main(void)
                     break;
             }
             break;
-        }
-        // Statemachine Switch
-        switch(current_state){
-        /// --- PACKAGE DETECTED --- ///
-        /*
-        case package_detected:
-            // Stop
-            task_manager(stop, standard_speed, standard_acceleration);
-            // Reset GP timer
-            gp_timer(-1);
-
-            if(RFID){// DEZE GAAT QUENTIN DOEN
-                // Add 1 to package count
-                package_metal++;
-            }
-            else{
-                package_non_metal++;
-            }
-            // Turn LEDs on
-            led_control(1);
-            // Make display extra bright
-            display_brightness(MAX_BRIGHTNESS);
-            // Play buzzer sound
-            while(play_beep()){
-                display_metal_and_non_metal(package_metal, package_non_metal);
-            }
-            // Remain stopped for set time
-            while(!gp_timer(DETECTION_STOP_TIME)){
-                display_metal_and_non_metal(package_metal, package_non_metal);
-            }
-            //THIS is for the change_demo_area. at package 5 for non metal
-            if (package_non_metal==change_demo_package)
-            {
-                current_state = einde_opdracht;
-            }
-            break;*/
-        case weight_detection:
-            //play_beep();
-            task_manager(forward_slow, standard_speed, standard_acceleration);
-            if (light_limit_switch_25() == 0) {
-                while(!gp_timer(DETECTION_STOP_TIME)){
-                    task_manager(forward_slow, standard_speed, standard_acceleration);
-            }
-                if ((light_limit_switch_25() == 0) &! (heavy_limit_switch_26() == 0)){
-                    current_state = light_weight;}
-                else{
-                    current_state = heavy_weight;}}
-
-
-            break;
-        case heavy_weight:
-            while(!gp_timer(DETECTION_STOP_TIME)){
-                    display_heavy();}
-            task_manager(stop, standard_speed, standard_acceleration);//Might need to be in timer, idk what the stop function does
-            current_state = reverse;
-
-
-            break;
-        case light_weight:
-            display_light();
-            task_manager(stop, standard_speed, standard_acceleration);
-            current_state = s_turn;
-            break;
-        case end: //einde bereikt
-            task_manager(stop, standard_speed, standard_acceleration);
-            display_end();
-            play_beep();
-            break;
-        case s_turn:
-            //maak S-bocht
-            //ja idk man
-
-            //als S-bocht klaar, AGV weer op stand 1
-            current_state = forward;//Deze kunnen we ook gwn programmering in s bocht zelf want verder wordt deze state niet gebruikt
-            break;
-        case reverse:
-            task_manager(backward_fast, standard_speed, standard_acceleration);
-            current_state = end;
-            break;
-        case forward:
-            //task_manager(forward_fast, standard_speed, standard_acceleration);//does AGV return an ack here?
-            if (light_limit_switch_25() == 0) {
-                current_state = weight_detection;
-            }
-
-        default: // Should be starting_state?
-            if(starting_button()){
-            task_manager(forward_fast, standard_speed, standard_acceleration);//does AGV return an ack here?
-            // Transition
-            current_state = forward;
-            }
-            break;
-
-
-        //display_statemachine(current_state);
+        } // end of statemachine switch
     } // end of Main loop //
 
     return 0;
-}} // end of Main //
+} // end of Main //
